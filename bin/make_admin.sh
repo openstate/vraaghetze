a61d95e3ceacd025b91f909cb038ec5728b5c8e1
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-08-25 11:16:37 +0200

    Refactors make_admin to use .env file

--- bin/make_admin.sh
+++ bin/make_admin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+source "$ROOT_DIR/.env"
+
 EMAIL=$1
 if [ -z "$EMAIL" ]; then
   echo "Usage: make_admin email_address"
@@ -6,10 +10,10 @@ if [ -z "$EMAIL" ]; then
   exit
 fi
 
-OLD_ROLE=`sudo docker exec vraaghetze-db-1 psql -t -U postgres -d vraaghetze -c "select role from public.user where email='$EMAIL';"`
+OLD_ROLE=`sudo docker exec vraaghetze-db-1 psql -t -U $POSTGRES_USER -d $POSTGRES_DB -c "select role from public.user where email='$EMAIL';"`
 OLD_ROLE=`echo $OLD_ROLE | xargs` # Trim spaces begin/end
 echo "Old role: $OLD_ROLE"
 
 echo "Now trying to set role to admin"
-RESULT=`sudo docker exec vraaghetze-db-1 psql -U postgres -d vraaghetze -c "update public.user set role='admin' where email='$EMAIL';"`
+RESULT=`sudo docker exec vraaghetze-db-1 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "update public.user set role='admin' where email='$EMAIL';"`
 echo $RESULT
