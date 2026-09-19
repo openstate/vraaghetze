e73c6b8512cdd57971ea156476162354ad102cee
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-09-19 14:39:49 +0200

    Fixes downloads of static files

--- src/routes/voorwaarden/+page.svelte
+++ src/routes/voorwaarden/+page.svelte
@@ -7,6 +7,6 @@
 <ContentPage title={data.meta.title}>
 	<p class="text-osf-canvas-600" >
 		Download de Algemene Voorwaarden
-		<a href="/static/files/algemene-voorwaarden.pdf">als pdf</a>.
+		<a href="/files/algemene-voorwaarden.pdf">als pdf</a>.
 	</p>
 </ContentPage>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { searchForWorkspaceRoot } from 'vite'
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	server: {
 		fs: {
 			allow: [
-				searchForWorkspaceRoot(process.cwd())
+				'static'
 			]
 		}
 	},
