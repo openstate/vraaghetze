3209718bce53e772a0db23c54d23180edc3304d8
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-09-19 13:09:55 +0200

    Adds temporary pdf file for Algemene Voorwaarden

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { searchForWorkspaceRoot } from 'vite'
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		fs: {
+			allow: [
+				searchForWorkspaceRoot(process.cwd())
+			]
+		}
+	},
 	test: {
 		include: ['src/**/*.test.ts'],
 		environment: 'node',
