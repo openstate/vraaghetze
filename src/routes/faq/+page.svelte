e87952b0b9ff54037af4152042f35bec9bb7b120
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-09-20 11:43:56 +0200

    Adds and changes contact information

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -45,5 +45,7 @@
 
 	<h2>Ik ben het niet eens met een moderatiekeuze, hoe kan ik bezwaar maken?</h2>
 
-	<p>TODO</p>
+	<p>Stuur een e-mail met je bezwaar naar
+		<a href="mailto:vraaghetze@openstate.eu" class="hover:underline">vraaghetze@openstate.eu</a>.
+	</p>
 </ContentPage>
