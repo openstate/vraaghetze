0cecf598b8e8525fbe7e3beb45224a853c64f53d
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2026-09-21 10:18:07 +0200

    Changes hovertext non-cooperating politicians

--- src/lib/politicians.ts
+++ src/lib/politicians.ts
@@ -1,5 +1,5 @@
 export function notAcceptingQuestionsText(name: string, email: string) {
-  return `Dit kamerlid heeft ervoor gekozen geen tijd te besparen door vragen via VraagHetZe te beantwoorden. Helaas voor jou, de vrager, en voor het kamerlid. Je kunt ${name} een bericht sturen via ${email}.`
+  return `Dit Kamerlid heeft ervoor gekozen niet openbaar antwoord te geven via VraagHetZe. Je kunt ${name} een bericht sturen via ${email}.`
 }
 
 export function preventAdding(event: Event, disable?: boolean) {
